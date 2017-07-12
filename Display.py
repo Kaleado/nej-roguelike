13d114605b87c94c4e2dbc938f7855d6bea48866
Emmet Murray <e.minor.murray@gmail.com>
2017-07-12 17:18:33 +1000

    Added display module with putch() and draw()

---
+++ Display.py
@@ -0,0 +1,37 @@
+import libtcodpy as libtcod
+
+
+# Takes a 2d aray and a starting x and y position, and draws the array on the map.
+# Arrays are organised as such:
+#
+# [ , , , , , , , ... ]    
+# [ , , , , , , , ... ]    ^
+#         ....             | Y axis
+# [ , , , , , , , ... ]    V
+# [ , , , , , , , ... ]
+#        <---> x axis
+#
+#     accessed like this:
+#      map[x][y]
+def draw(startx, starty, data):
+	# Case for single char
+	if type(data) is str:
+    	libtcod.console_put_char(0, startx, starty, data[0], libtcod.BKGND_NONE)
+	else:
+		# Case for 2d array
+		for i in range(0, len(data)):
+			for j in range(0, len(data[i])):
+				libtcod.console_put_char(0, startx + i, starty + j, data[i][j], libtcod.BKGND_NONE)
+
+def putch(char, x, y):
+	# Check if the dict of colours exists
+	if 'colors' in locals():
+		# Print found color, otherwise white
+		if char in colors:
+			libtcod.console_set_default_foreground(0, colors[char])
+			libtcod.console_put_char(0, x, y, char, libtcod.BKGND_NONE)
+			libtcod.console_set_default_foreground(0, libtcod.white)
+		else:
+			libtcod.console_set_default_foreground(0, libtcod.white)
+			libtcod.console_put_char(0, x, y, char, libtcod.BKGND_NONE)
+			
